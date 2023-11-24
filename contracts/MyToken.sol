b31a12149a222bce9217c8a126acf26ff4695bc6
sonnesen <sonnesen@gmail.com>
2023-11-24 00:11:31 -0300

    First commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,5 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+    constructor(uint256 initialSupply_) ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        _mint(msg.sender, initialSupply_);        
+    }
 }
